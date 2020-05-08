bc3937eefbbf345436ae37e6256b017e7b435790
KhoaLe <kle@digication.com>
2020-05-08 03:40:36 +0200

    cast type

--- arae/pan/semeval2019/test.py
+++ arae/pan/semeval2019/test.py
@@ -114,8 +114,7 @@ def eval_classifier(classifier1, classifier2, whichclass, batch):
   scores2 = scores2.squeeze(1)
   scores = scores1 > (1-scores2)
 
-  pred = scores.data.round().squeeze(1)
-  accuracy = pred.eq(labels.data).float().mean()
+  accuracy = scores.float().data.eq(labels.data).float().mean()
 
   return accuracy
 
